979bb37a1ade9f61a95806fadbf8e0bf29546184
Haidar Hammoud <haidar.hmd1@gmail.com>
2022-02-28 23:19:52 +0100

    refactor app.js

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ import * as THREE from "three";
 // Set consts
 const SCREEN_WIDTH = window.innerWidth;
 const SCREEN_HEIGHT = window.innerHeight;
-const TWO_PI = Math.PI / 2;
 
+/* INIT */
 // create a scene with a webGL renderer and a camera
 const scene = new THREE.Scene();
 
@@ -27,27 +27,14 @@ camera.position.set(0, 2.5, 10);
 
 // add a grid helper to the scene
 const helper = new THREE.GridHelper();
-helper.rotation.x = TWO_PI;
 scene.add(helper);
 
 // create an object
-// const geometry = new THREE.BoxGeometry();
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ffff });
-// const cube = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ffff });
+const cube = new THREE.Mesh(geometry, material);
 
-// scene.add(cube);
-
-const dir = new THREE.Vector3(1, 2, 0);
-
-//normalize the direction vector (convert to vector of length 1)
-dir.normalize();
-
-const origin = new THREE.Vector3(0, 0, 0);
-const length = 1;
-const hex = 0xffff00;
-
-const arrowHelper = new THREE.ArrowHelper(dir, origin, length, hex);
-scene.add(arrowHelper);
+scene.add(cube);
 
 // create function to resize the canvas whenever the screen changes
 window.addEventListener("resize", onWindowResize);
@@ -56,6 +43,8 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 }
+
+/* END INIT */
 
 // add the "game loop" function -> should be a recursive function hold by requestAnimationFrame
 function animate() {
